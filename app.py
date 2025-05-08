f574dac4cab54a8036e5fe262eff746bade392f0
dinusugathan <dinuts1997@gmail.com>
2025-05-08 13:28:18 +0530

    app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ genai.configure(api_key=GENAI_KEY)
 model = genai.GenerativeModel('gemini-2.0-flash')
 
 # Load and initialize the sentence transformer model
-sentence_model = SentenceTransformer('all-MiniLM-L6-v2', device='cpu')
+sentence_model = SentenceTransformer('all-MiniLM-L6-v2')
 # Load course and instructor data from JSON files
 with open("data/courses.json", "r") as f:
     courses = json.load(f)
